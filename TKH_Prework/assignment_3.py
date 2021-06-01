4acf9aefb17b10cdaa2ef1515e30bfadc05fafe5
LinqingZ <72286194+LinqingZ@users.noreply.github.com>
2021-06-01 01:19:49 -0400

    Update assignment_3.py

--- TKH_Prework/assignment_3.py
+++ TKH_Prework/assignment_3.py
@@ -1,10 +1,9 @@
 print(len("hello"))
 name="joe"
 print(len(name))
-
+longest_name=""
 names_list=["bob","jimmy", "max b","bernie","jordan","future hendrix"]
 for name in names_list:
-    longest_name=""
     if len(name)>len(longest_name):
         longest_name=name
 print(longest_name)
