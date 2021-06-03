045e8543f1c8d1a94ece2a27e26e6ec4da9e0608
LinqingZ <72286194+LinqingZ@users.noreply.github.com>
2021-06-03 12:59:58 -0400

    Update assignment_5.py

--- TKH_Prework/assignment_5.py
+++ TKH_Prework/assignment_5.py
@@ -6,14 +6,14 @@ print(list[2])
 
 even=0
 odd=1
-even_list=[]
-odd_list=[]
+b=[]
+c=[]
 names_list = ["bob","jimmy","max b", "bernie", "jordan", "future hendrix"]
 for i in range(0, len(names_list)):
     if i % 2:
-        even_list.append(names_list[i])
+        b.append(names_list[i])
     else:
-        odd_list.append(names_list[i])
+        c.append(names_list[i])
         
-print("even list= "+str(even_list))
-print("odd list= "+str(odd_list))
+print("even list= "+str(b))
+print("odd list= "+str(c))
